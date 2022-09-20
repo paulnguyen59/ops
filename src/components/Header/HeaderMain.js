2938e8ece42f0ddb3a31e520c37d10253459ceaa
Nguyễn Ngọc Duy Anh <anhnnd@matbao.com>
2022-09-20 17:30:08 +0700

    update responsive

--- src/components/Header/HeaderMain.js
+++ src/components/Header/HeaderMain.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../Header/logo.png'
+import logo2 from '../Header/logo2.png'
 import { NavLink, Link } from "react-router-dom";
 const HeaderMain = () => {
     return (
--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import slider1 from './slide1.jpg'
 import slider2 from './slider2.jpg'
+import slideBg from './slidebg2.jpg'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './Home.scss'
@@ -17,10 +18,30 @@ const Homepage = () => {
         slidesToShow: 1,
         slidesToScroll: 1
     };
+
     return (
         <>
-            <section id='section-hero'>
-                <div className="slider-list">
+            <section id='section-hero' className='section' style={{ backgroundImage: `url(${slideBg})` }}>
+                <div className="hero-content">
+                    <div className="container">
+                        <form>
+                            <div className="hero-form-top">
+                                <div className="form-group">
+                                   
+                                    <input type="text" className='form-control' placeholder='Tìm kiếm hạng mục đầu tư' />
+                                </div>
+                                <button className='btn btn-primary'>Tìm kiếm</button>
+                            </div>
+
+                            <div className="form-group">
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="bg-overlay"></div>
+                {/* <div className="slider-list">
                     <Slider {...settings}>
                         <div className="slider-item">
                             <img src={slider1} />
@@ -29,12 +50,12 @@ const Homepage = () => {
                             <img src={slider2} />
                         </div>
                     </Slider>
-                </div>
+                </div> */}
             </section>
             <Land></Land>
             <ProjectList></ProjectList>
-            
-           
+
+
         </>
     );
 };
